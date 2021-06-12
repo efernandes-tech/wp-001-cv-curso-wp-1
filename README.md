571ec371206bd04e2c906d11ce4d4e18344c63cd
Éderson Fernandes <edersonluis.rf@gmail.com>
2021-06-12 16:27:33 -0300

    curso de wordpress 08 - instalando temas e plugins

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # wp-001-cv-curso-wp-1
+
+## Notes:
+
+- Melhores temas (remover o q ñ está usando):
+
+    - Astra (não ativar o tema principal)
+    - Gerar tema filho:
+
+        - https://wpastra.com/child-theme-generator/
+
+- Melhores plugins (remover o q ñ está usando):
+
+    - Gutenberg Blocks – Ultimate Addons for Gutenberg (ativar)
+    - Microthemer Lite – Visual Editor to Customize CSS (ativar)
+    - Editor clássico (ativar e config)
+    - Astra Widgets
+    - Companion Auto Update
+    - EWWW Image Optimizer (ativar)
+    - MonsterInsights – Google Analytics Dashboard for WordPress (Website Stats Made Easy)
+    - Importador do WordPress
+    - Rank Math SEO – Best SEO Plugin For WordPress To Increase Your SEO Traffic
+    - Really Simple SSL
+    - UpdraftPlus WordPress Backup Plugin
+    - WP Mail SMTP by WPForms
+    - WP Super Cache
+    - Contact Form by WPForms – Drag & Drop Form Builder for WordPress (ativar)
+    - WPS Hide Login (ativar)
---
+++ cursowp/wp-content/plugins/seo-by-rank-math/vendor/composer/installed.php
@@ -0,0 +1,69 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c3b8bdc234cf34a0980a1cf78a67b9cf0cb5a569',
+    'name' => 'rankmath/seo-by-rank-math',
+  ),
+  'versions' => 
+  array (
+    'a5hleyrich/wp-background-processing' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2cbee1abd1b49e1133cd8f611df4d4fc5a8b9800',
+    ),
+    'cmb2/cmb2' => 
+    array (
+      'pretty_version' => 'v2.9.0',
+      'version' => '2.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cacbc8cedbfdf8ffe0e840858e6860f9333c33f2',
+    ),
+    'donatj/phpuseragentparser' => 
+    array (
+      'pretty_version' => 'v1.4.0',
+      'version' => '1.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '246c1cf0a44f07168c702203bf30d5f48f17bab0',
+    ),
+    'mythemeshop/wordpress-helpers' => 
+    array (
+      'pretty_version' => 'v1.1.13',
+      'version' => '1.1.13.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5f291a421ecb7b258f522b53427fc4ce552d8851',
+    ),
+    'rankmath/seo-by-rank-math' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c3b8bdc234cf34a0980a1cf78a67b9cf0cb5a569',
+    ),
+    'woocommerce/action-scheduler' => 
+    array (
+      'pretty_version' => '3.1.6',
+      'version' => '3.1.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '12c91fd20af0cdfb6107b8db9df78d133f512b04',
+    ),
+  ),
+);
